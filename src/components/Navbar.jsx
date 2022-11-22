4577b4b8e64ac3ba1877d0ecbdf9f495c89c6dcf
Carla Willmes <willmes.carla@gmail.com>
2022-11-22 17:34:07 +0900

    added navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <img src="./src/images/airbnb-logo.png" />
+      <p>Welcome</p>
+    </>
+  )
+}
+
+export default Navbar
