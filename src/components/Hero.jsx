376401d304cd88ded1b3a0eaa11ce77749f8182e
Carla Willmes <willmes.carla@gmail.com>
2022-11-23 16:15:37 +0900

    styled hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className="hero">
+      <img className="hero--img" src="./src/images/image-grid.png" />
+      <h1 className="hero--title">Online Experiences</h1>
+      <p className="hero--p">Join unique interactive activities led by one-of-a-kind hosts – all without leaving home.</p>
+    </section>
+  )
+}
+
+export default Hero
