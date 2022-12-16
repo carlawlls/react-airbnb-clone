315da9e5801f18a6ac619283fc26e74a3a6088ee
Carla Willmes <willmes.carla@gmail.com>
2022-12-16 17:00:39 +0900

    added data to card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,29 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import Data from './Data'
 
 export default function App() {
   const [count, setCount] = useState(0)
+
+  const cards = Data.map(item => {
+    return (
+      <Card
+          img={item.coverImg}
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          location={item.location}
+          title={item.title}
+          price={item.price}
+      />
+    )
+    })
 
   return (
     <div>
         <Navbar />
         <Hero />
-        <Card
-            img="card.png"
-            rating="5.0"
-            reviewCount={6}
-            country="USA"
-            title="Life Lessons with Katie Zaferes"
-            price={136}
-        />
+        {cards}
     </div>
   )
 }
