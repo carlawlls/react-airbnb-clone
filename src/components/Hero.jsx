c195882424dfdb19a3791bc5ffb8985e195e3d0c
Carla Willmes <willmes.carla@gmail.com>
2022-12-18 13:31:20 +0900

    fixed image path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Hero() {
   return (
       <section className="hero">
-          <img src="./public/images/image-grid.png" className="hero--photo" />
+          <img src="../public/images/image-grid.png" className="hero--photo" />
           <div className="hero--info">
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by
